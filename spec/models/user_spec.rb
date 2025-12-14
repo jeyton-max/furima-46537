426a995385c67d7233c38033a01b07784f9af944
jeyton-max <junaiko39@gmail.com>
2025-12-14 17:37:41 +0900

    Userモデル単体テストコード実装完了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "email、password、password_comfirmation、nickname、last_name、first_name、last_name_kana、first_name_kana、birth_dateが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+    end
+    
+  end
 end
