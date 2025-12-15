839e2ed70ab0634d4bab5896529659f61bd244ca
jeyton-max <junaiko39@gmail.com>
2025-12-15 10:09:26 +0900

    Rubocopによるコードスタイルの自動修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.last_name }
     email { Faker::Internet.email }
-    #英数字混合で合計文字数が6文字以上となるように設定しています。
-    password { 'a1' + Faker::Internet.password(min_length: 4) }
+    # 英数字混合で合計文字数が6文字以上となるように設定しています。
+    password { "a1#{Faker::Internet.password(min_length: 4)}" }
     password_confirmation { password }
 
     # 全角（漢字・ひらがな・カタカナ）のバリデーションを通過させるため、固定値としています。
