2fc2103c18911863d544e942c531fd0fccd6d268
jeyton-max <junaiko39@gmail.com>
2025-12-18 14:29:03 +0900

    Itemモデルの正常系テストコード実装完了

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品できる場合' do
+      it 'image、name、description、price、category_id、condition_id、shipping_fee_id、prefecture_id、shipping_day_idが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+  end
 end
