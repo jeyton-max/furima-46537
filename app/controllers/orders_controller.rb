70b76b6ef6e71cc6380c6a509734160886b44fe2
jeyton-max <junaiko39@gmail.com>
2025-12-27 12:37:05 +0900

    商品購入機能のコントローラー定義、決済関連のGem導入
    ・ordersコントローラーのルーティング設定（resourcesメソッドでindex,createアクションを適用）
    ・ordersコントローラーのindex,createアクションを定義、beforeアクション定義（privateセクションのset_item,move_to_index、authenticate_user!によるログイン認証）
    ・items/showビューの購入画面に進むリンク先のパスヘルパー指定
    ・クレジットカード決済機能に関するGem導入（payjp,gon）
    ・orders/indexビューにgonを読み込む記述を追加

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
+
+  def index
+  end
+
+  def create
+  end
+
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if current_user.id == @item.user_id || @item.order.present?
+      redirect_to root_path
+    end
+  end
+end
