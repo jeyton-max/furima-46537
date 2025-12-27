2be83dbd3ca600c6b2d8eb6f424f309d815e3f28
jeyton-max <junaiko39@gmail.com>
2025-12-27 16:44:09 +0900

    商品購入機能のFormオブジェクト（OrderAddress）実装
    ・order_address.rbファイルにOrderAddressクラスを定義
    ・OrderAddressクラスにActiveModel::Modelをinclude
    ・attr_accessor設定（保存カラムと属性値の指定）
    ・Formオブジェクトのバリデーション設定（form_withメソッド対応）

---
+++ app/models/order_address.rb
@@ -0,0 +1,21 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postcode, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
+
+  with_options presence: true do
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city, :house_number, :user_id, :item_id
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid" }
+    validates :token
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    ActiveRecord::Base.transaction do
+      order = Order.create!(user_id: user_id, item_id: item_id)
+      Address.create!(postcode: postcode, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    end
+  rescue ActiveRecord::RecordInvalid
+    false
+  end
+end
