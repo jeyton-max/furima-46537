ed81f48ed9766bef57f1f9f8a7db4c31f5713c40
jeyton-max <junaiko39@gmail.com>
2026-01-02 11:07:47 +0900

    itemsコントローラー/move_to_index 条件分岐処理の実装
    ・itemsコントローラーのprivateセクション（move_to_index）に条件分岐追加※ログインユーザーが出品者ではない、もしくは購入記録のある商品は編集ページにアクセスできない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,8 +51,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return if @item.user_id == current_user.id
+    if current_user.id != @item.user_id || @item.order.present?
 
     redirect_to root_path
+    end
   end
 end
