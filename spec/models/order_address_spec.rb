d07fa5b5287ad4bc09b4621b0cf9a8175ed98544
jeyton-max <junaiko39@gmail.com>
2026-01-02 10:03:21 +0900

    order_address(Formオブジェクト)の異常系単体テストコード実装
    ・order_address(Formオブジェクト）の異常系テストコード実装（exampleの洗い出し～テスト実行）
    ・トランザクション二重管理による接続エラー回避（spec/rails_helper.rbのconfig.use_transactional_fixtures設定をfalseに変更）
    ・RuboCop実行によるコード修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -18,5 +18,103 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address).to be_valid
       end
     end
+
+    context '商品購入できない場合' do
+      it 'postcodeが空では購入できない' do
+        @order_address.postcode = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postcode can't be blank")
+      end
+
+      it 'cityが空では購入できない' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+
+      it 'house_numberが空では購入できない' do
+        @order_address.house_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("House number can't be blank")
+      end
+
+      it 'phone_numberが空では購入できない' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it 'tokenが空では購入できない' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+
+      it 'prefecture_idが1では購入できない' do
+        @order_address.prefecture_id = 1
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'user_idが空では購入できない' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'item_idが空では購入できない' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+
+      it 'postcodeにハイフンが含まれていないと購入できない' do
+        @order_address.postcode = '0123456'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Postcode is invalid. Include hyphen(-)')
+      end
+
+      it 'postcodeが全角の数値だと購入できない' do
+        @order_address.postcode = '０１２-３４５６'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Postcode is invalid. Include hyphen(-)')
+      end
+
+      it 'postcodeに英字などが含まれていると購入できない' do
+        @order_address.postcode = 'O12-3456'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Postcode is invalid. Include hyphen(-)')
+      end
+
+      it 'phone_numberにハイフンが含まれていると購入できない' do
+        @order_address.phone_number = '012-3456-7890'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+
+      it 'phone_numberが全角の数値だと購入できない' do
+        @order_address.phone_number = '０１２３４５６７８９０'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+
+      it 'phone_numberに英字などが含まれていると購入できない' do
+        @order_address.phone_number = 'O1234567890'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+
+      it 'phone_numberが9桁以下の数値では購入できない' do
+        @order_address.phone_number = '012345678'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+
+      it 'phone_numberが12桁以上の数値では購入できない' do
+        @order_address.phone_number = '810123456789'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+    end
   end
 end
