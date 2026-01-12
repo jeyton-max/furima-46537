4582ea951dc032a287ce2adc5198a86612d89fcd
jeyton-max <junaiko39@gmail.com>
2026-01-12 19:55:32 +0900

    自動デプロイ実行のための補完
    ・config/deploy.rbに使用ブランチの指定を追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,9 @@ set :application, 'furima-46537'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:jeyton-max/furima-46537.git'
 
+# 追加：使用するブランチを指定
+set :branch, 'main'
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
